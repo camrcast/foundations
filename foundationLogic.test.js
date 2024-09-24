d70c042c4feb760a987b844dc7abf8423e3be2d7
camrcast <camrcast@gmail.com>
2024-09-24 08:53:40 -0700

    Enough tests and logic fully works

--- foundationLogic.test.js
+++ foundationLogic.test.js
@@ -1,17 +1,2 @@
-const {validateReg, validateLogin, authenticateToken, authenticateManagerToken, createToken, processTicket, validateStatus, getTickets} = require("./foundationLogic");
+const {createNewUser, validateLogin, authenticateToken, authenticateManagerToken, createToken, processTicket, validateStatus} = require("./foundationLogic");
 
-describe('Logic Tests', () => {
-  beforeEach(() => {
-    validateReg.mockClear();
-  });
-
-  test('should add to database', () => {
-    const username = "Hello";
-    const password = "world";
-    const role = "Employee";
-    
-    validateReg(username, password, role);
-
-    expect(fs.writeFileSync).toHaveBeenCalledWith(filePath, JSON.stringify(shoppingList, null, 2));
-  });
-});
